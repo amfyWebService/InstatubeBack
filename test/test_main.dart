63c545267d869624e9133bed61d37935a8617af0
Goms <gomesdpro@gmail.com>
2019-10-04 17:16:41 +0200

    Coveralls

---
+++ test/test_main.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import 'healthcheck_test.dart' as healthcheck_test;
+
+void main() {
+  group('healthcheck_test.dart', healthcheck_test.main);
+}
