a72de8e81d37d2ce5edef668349d1a79ecf17d85
Goms <gomesdpro@gmail.com>
2019-10-11 17:11:27 +0200

    deploy !!!

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "cat \$HOME/deploy.sh"
-# ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo \$HOME/deploy.sh"
-ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo /home/ubuntu/deploy.sh"
+ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo sh \$HOME/deploy.sh"
+# ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo /home/ubuntu/deploy.sh"
 
 exit 0
