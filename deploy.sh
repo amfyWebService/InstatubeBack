db6a65105013ff41a074843221b47e8f62df3f26
Goms <gomesdpro@gmail.com>
2019-10-11 17:32:32 +0200

    YES !

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "cat \$HOME/deploy.sh"
 ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo sh \$HOME/deploy.sh"
-# ssh -i ./clemongo ubuntu@$SERVER_IP_ADDRESS "sudo /home/ubuntu/deploy.sh"
 
 exit 0
